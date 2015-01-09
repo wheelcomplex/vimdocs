257210bd3af3dd8a610319dddb9af41ac3b604bb
YinJiaJin <yinjiajin@xiaomi.com>
2015-01-09 14:15:46 +0800

    fix if error, add bootstrap tip

--- vim-ubuntu-setup-for-go.sh
+++ vim-ubuntu-setup-for-go.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 #
+# bootstrap:
+# cd ~ && wget 'https://raw.githubusercontent.com/wheelcomplex/vimdocs/master/vim-ubuntu-setup-for-go.sh' -O vim-ubuntu-setup-for-go.sh && chmod +x vim-ubuntu-setup-for-go.sh && ./vim-ubuntu-setup-for-go.sh
+#
 # base on https://github.com/yourihua/Documents/blob/master/Vim/Mac%E4%B8%8B%E4%BD%BF%E7%94%A8Vim%E6%90%AD%E5%BB%BAGo%E5%BC%80%E5%8F%91%E7%8E%AF%E5%A2%83.mdown
 #
 
+if [ "$VIMSETUPNEW" != 'YES' ]
+then
+gcmd="git clone https://github.com/wheelcomplex/vimdocs.git ~/tmp/vimdocs/"
+rm -rf ~/tmp/vimdocs && mkdir -p ~/tmp/ && $gcmd
+if [ $? -ne 0 ]
+	then
+	echo "error: git clone failed: $gcmd"
+	exit 1
+fi
+    export VIMSETUPNEW='YES'
+    gcmd="~/tmp/vimdocs/`basename $0`"
+    echo "Run script from git: $gcmd"
+    $gcmd
+    exit $?
+fi
+
 cat /etc/issue.net
 isubuntu=`cat /etc/issue.net| grep -ci Ubuntu`
-if $isubuntu -eq 0 ]
+if [ $isubuntu -eq 0 ]
 	then
 	echo "error: this script support ubuntu only."
 	exit 1
@@ -23,7 +42,7 @@ if [ ! -x /usr/bin/vim ]
 fi
 
 vimverok=`/usr/bin/vim --version | grep -c 'VIM - Vi IMproved 7.'`
-if $vimverok -eq 0 ]
+if [ $vimverok -eq 0 ]
 	then
 	echo "error: this script support VIM - Vi IMproved 7.x only."
 	echo "TIPS: sudo apt-get update && sudo apt-get install vim"
@@ -31,7 +50,7 @@ if $vimverok -eq 0 ]
 fi
 
 cmdok=`which git| wc -l`
-if $cmdok -eq 0 ]
+if [ $cmdok -eq 0 ]
 	then
 	echo "error: git has not installed"
 	echo "TIPS: sudo apt-get update && sudo apt-get install git"
@@ -39,7 +58,7 @@ if $cmdok -eq 0 ]
 fi
 
 cmdok=`which hg| wc -l`
-if $cmdok -eq 0 ]
+if [ $cmdok -eq 0 ]
 	then
 	echo "error: hg/mercurial has not installed"
 	echo "TIPS: sudo apt-get update && sudo apt-get install mercurial"
@@ -47,7 +66,7 @@ if $cmdok -eq 0 ]
 fi
 
 cmdok=`env | grep -c 'GOARCH='`
-if $cmdok -eq 0 ]
+if [ $cmdok -eq 0 ]
 	then
 	echo "error: GOARCH environment has not setup"
 	echo "TIPS: http://wiki.ubuntu.org.cn/Golang"
@@ -56,7 +75,7 @@ if $cmdok -eq 0 ]
 fi
 
 cmdok=`env | grep -c 'GOROOT='`
-if $cmdok -eq 0 ]
+if [ $cmdok -eq 0 ]
 	then
 	echo "error: GOROOT environment has not setup"
 	echo "TIPS: http://wiki.ubuntu.org.cn/Golang"
@@ -65,7 +84,7 @@ if $cmdok -eq 0 ]
 fi
 
 cmdok=`env | grep -c 'GOPATH='`
-if $cmdok -eq 0 ]
+if [ $cmdok -eq 0 ]
 	then
 	echo "error: GOPATH environment has not setup"
 	echo "TIPS: http://wiki.ubuntu.org.cn/Golang"
@@ -85,8 +104,9 @@ if [ $needback -ne 0 ]
 	then
 	mv ~/.vim* ~/"$backdir"
 fi
+
 gcmd="git clone https://github.com/wheelcomplex/vimdocs.git ~/tmp/vimdocs/"
-mkdir -p ~/tmp/ && $gcmd
+rm -rf ~/tmp/vimdocs && mkdir -p ~/tmp/ && $gcmd
 if [ $? -ne 0 ]
 	then
 	echo "error: git clone failed: $gcmd"
